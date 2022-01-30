904b867f078646ad63f44048beb4fd275c743640
Timofey Katkov <katkov0103@yandex.ru>
2022-01-30 21:11:44 +1000

    Deleted swap dir

--- swap/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer
-from database import Base
-
-class Blockchain(Base):
-    __tablename__ = "blockchain"
-
-    id = Column(Integer, primary_key=True, index=True)
-    last_bsc_block = Column(Integer)
-    last_polygon_block = Column(Integer)
